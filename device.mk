f03d289a278f3c96102a41659f2d4cf31ec90ef9
Furkan Enes <113352008+QCerberusQ@users.noreply.github.com>
2026-01-24 13:07:37 +0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -11,14 +11,14 @@ LOCAL_PATH := device/nothing/Spacewar
 
 
 # A/B
-AB_OTA_POSTINSTALL_CONFIG += \
-    RUN_POSTINSTALL_system=true \
-    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
-    FILESYSTEM_TYPE_system=ext4 \
-    POSTINSTALL_OPTIONAL_system=true
+#AB_OTA_POSTINSTALL_CONFIG += \
+#    RUN_POSTINSTALL_system=true \
+#    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+#    FILESYSTEM_TYPE_system=ext4 \
+#    POSTINSTALL_OPTIONAL_system=true
 
-PRODUCT_PACKAGES += \
-    otapreopt_script
+#PRODUCT_PACKAGES += \
+#    otapreopt_script
 
 # Boot control HAL
 PRODUCT_PACKAGES += \
