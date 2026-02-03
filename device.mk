f7ad4b3a2ac419bdfd3ad5b4aaa025e774d028c7
Furkan Enes <113352008+QCerberusQ@users.noreply.github.com>
2026-02-03 19:02:34 +0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -6,42 +6,24 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
-
-LOCAL_PATH := device/nothing/Spacewar
-
-PRODUCT_TARGET_VNDK_VERSION := 30
-
-# A/B
-#AB_OTA_POSTINSTALL_CONFIG += \
-#    RUN_POSTINSTALL_system=true \
-#    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
-#    FILESYSTEM_TYPE_system=ext4 \
-#    POSTINSTALL_OPTIONAL_system=true
-
-#PRODUCT_PACKAGES += \
-#    otapreopt_script
-
-# Boot control HAL
-PRODUCT_PACKAGES += \
-    android.hardware.boot@1.1-impl-qti.recovery \
-    bootctrl.lahaina.recovery
-
-PRODUCT_PACKAGES_DEBUG += \
-    bootctl
-
 # Dynamic partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
-# Fastbootd
-PRODUCT_PACKAGES += \
-    android.hardware.fastboot@1.0-impl-mock \
-    fastbootd
+LOCAL_PATH := device/nothing/Spacewar
 
-# Soong namespaces
-PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH) \
-    hardware/qcom-caf/bootctrl \
-    vendor/qcom/opensource/commonsys-intf/display
+PRODUCT_SHIPPING_API_LEVEL := 31
+
+PRODUCT_TARGET_VNDK_VERSION := 31
+
+# A/B
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+    FILESYSTEM_TYPE_system=ext4 \
+    POSTINSTALL_OPTIONAL_system=true
+
+PRODUCT_PACKAGES += \
+    otapreopt_script
 
 # Update engine
 PRODUCT_PACKAGES += \
@@ -52,90 +34,49 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES_DEBUG += \
     update_engine_client
 
-# TWRP / OrangeFox config
-#TW_THEME := portrait_hdpi
-RECOVERY_SDCARD_ON_DATA := true
-TARGET_RECOVERY_QCOM_RTC_FIX := true
-TW_EXCLUDE_DEFAULT_USB_INIT := true
-TW_EXTRA_LANGUAGES := true
-TW_INCLUDE_NTFS_3G := true
-TW_USE_TOOLBOX := true
-TW_INCLUDE_RESETPROP := true
-TW_INCLUDE_REPACKTOOLS := true
-TW_INPUT_BLACKLIST := "hbtp_vm"
-TW_BRIGHTNESS_PATH := "/sys/class/backlight/panel0-backlight/brightness"
-TWRP_INCLUDE_LOGCAT := true
-TARGET_USES_LOGD := true
-TARGET_USES_MKE2FS := true
-TW_NO_SCREEN_BLANK := true
-TW_EXCLUDE_APEX := true
-TW_CUSTOM_CPU_TEMP_PATH := "/sys/devices/virtual/thermal/thermal_zone50/temp"
-TW_CUSTOM_BATTERY_PATH := "/sys/class/power_supply/battery"
-# Display brightness (OrangeFox requires this)
-TW_MAX_BRIGHTNESS := 2047
-TW_DEFAULT_BRIGHTNESS := 1200
-
-
-#TW_LOAD_VENDOR_MODULES := \
-#    q6_pdr_dlkm.ko \
-#    q6_notifier_dlkm.ko \
-#    snd_event_dlkm.ko \
-#    apr_dlkm.ko \
-#    adsp_loader_dlkm.ko \
-#    msm_drm.ko \
-#    fts_tp.ko
-
-#TW_LOAD_VENDOR_MODULES_EXCLUDE_GKI := true
-
-# A/B
-AB_OTA_UPDATER := true
-
-AB_OTA_PARTITIONS += \
-    boot \
-    dtbo \
-    odm \
-    product \
-    system \
-    system_ext \
-    vbmeta \
-    vbmeta_system \
-    vendor
-
-
-# Crypto
-TW_INCLUDE_CRYPTO := true
-TW_INCLUDE_CRYPTO_FBE := true
-TW_INCLUDE_FBE_METADATA_DECRYPT := true
-BOARD_USES_QCOM_FBE_DECRYPTION := true
-
-# Haptics fix
-TW_SUPPORT_INPUT_AIDL_HAPTICS_FIX_OFF := true
-
-TARGET_RECOVERY_DEVICE_MODULES += \
-    libandroidicu \
-    libdisplayconfig.qti \
-    libion \
-    vendor.display.config@1.0 \
-    vendor.display.config@2.0 \
-    libdisplayconfig.qti
-
-RECOVERY_LIBRARY_SOURCE_FILES += \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libion.so \
-    $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/libdisplayconfig.qti.so \
-    $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@1.0.so \
-    $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@2.0.so
-
-# Keystore
+# Boot control HAL
 PRODUCT_PACKAGES += \
-    android.system.keystore2 \
+    bootctrl.lahaina.recovery \
+    android.hardware.boot@1.1-impl-qti.recovery
+
+PRODUCT_PACKAGES_DEBUG += \
+    bootctl
+
+#  Health
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl.recovery \
+    android.hardware.health@2.1-service
+
+# Fastbootd
+TW_INCLUDE_FASTBOOTD := true
+
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.1-impl-mock
+
+# Screen
+TARGET_SCREEN_HEIGHT := 2400
+TARGET_SCREEN_WIDTH := 1080
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH) \
+    hardware/qcom-caf/bootctrl
+
+PRODUCT_PACKAGES += \
     qcom_decrypt \
     qcom_decrypt_fbe
 
-# Health HAL
-PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl \
-    android.hardware.health@2.1-service \
-    libhealthd.$(PRODUCT_PLATFORM)
+PRODUCT_COPY_FILES += \
+    $(OUT_DIR)/target/product/$(PRODUCT_RELEASE_NAME)/obj/SHARED_LIBRARIES/libandroidicu_intermediates/libandroidicu.so:$(TARGET_COPY_OUT_RECOVERY)/root/system/lib64/libcuuc.so
 
-# VINTF
-PRODUCT_ENFORCE_VINTF_MANIFEST := true
+TARGET_RECOVERY_DEVICE_MODULES += \
+    libion \
+    libxml2 \
+    libandroidicu
+
+RECOVERY_LIBRARY_SOURCE_FILES += \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libion.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libxml2.so
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.product.device=$(PRODUCT_RELEASE_NAME)
