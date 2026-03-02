c99a28da47a8cd15560861ebaf964429ccd73a88
QCerberusQ <113352008+QCerberusQ@users.noreply.github.com>
2026-03-03 01:26:47 +0300

    Override System Properties

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -210,9 +210,13 @@ TW_USE_HEALTH_SERVICES_FOR_BATTERY := true
 #TW_CUSTOM_BATTERY_PATH := "/sys/class/power_supply/battery"
 TW_SUPPORT_INPUT_AIDL_HAPTICS_FIX_OFF := true
 TW_INCLUDE_FASTBOOTD := true
+
 # -----------------------------------------------------------------------------
 # Modules Load
 # -----------------------------------------------------------------------------
-
 TW_LOAD_VENDOR_MODULES := $(shell echo \"$(shell ls $(DEVICE_PATH)/recovery/root/vendor/lib/modules)\")
 
+# -----------------------------------------------------------------------------
+# Override System Properties
+# -----------------------------------------------------------------------------
+TW_OVERRIDE_SYSTEM_PROPS := "ro.build.date.utc;ro.build.product;ro.build.fingerprint=ro.system.build.fingerprint;ro.build.version.incremental;ro.product.device=ro.product.system.device;ro.product.model=ro.product.system.model;ro.product.name=ro.product.system.name"
