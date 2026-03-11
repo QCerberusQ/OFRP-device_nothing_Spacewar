877ffa26ed83cda7f9cf212dc4cbd55e40cc8c9a
QCerberusQ <113352008+QCerberusQ@users.noreply.github.com>
2026-03-12 00:47:40 +0300

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -24,7 +24,6 @@ export FOX_VANILLA_BUILD=1
 # A/B & Virtual AB
 export FOX_AB_DEVICE=1
 export FOX_VIRTUAL_AB_DEVICE=1
-export FOX_VENDOR_BOOT_RECOVERY=1
 
 # -----------------------------------------------------------------------
 # Storage & Format Rescuers (Bootloop Fixers)
