6bf906007b873de0cd6d90d76329433a16c1189e
QCerberusQ <113352008+QCerberusQ@users.noreply.github.com>
2026-03-22 01:38:10 +0300

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -226,7 +226,7 @@ TW_INCLUDE_FASTBOOTD := true
 # -----------------------------------------------------------------------------
 # Modules Load
 # -----------------------------------------------------------------------------
-TW_LOAD_VENDOR_MODULES := $(shell echo \"$(shell ls $(DEVICE_PATH)/prebuilt/modules)\")
+TW_LOAD_VENDOR_MODULES := "adsp_loader_dlkm.ko apr_dlkm.ko fts_tp.ko goodix_fp.ko q6_dlkm.ko q6_notifier_dlkm.ko q6_pdr_dlkm.ko snd_event_dlkm.ko swr_haptics_dlkm.ko"
 
 # -----------------------------------------------------------------------------
 # Override System Properties
