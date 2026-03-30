7ba290a0a5205a31da44493132d85290dbb300f7
QCerberusQ <113352008+QCerberusQ@users.noreply.github.com>
2026-03-30 16:49:44 +0300

    Clean up copyright notices in twrp_Spacewar.mk
    Removed outdated copyright information from the file.

--- twrp_Spacewar.mk
+++ twrp_Spacewar.mk
@@ -1,11 +1,5 @@
 #
-# Copyright (C) 2025 The Android Open Source Project
-# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
-#
 # SPDX-License-Identifier: Apache-2.0
-#
-# Copyright (C) 2024-2026 The OrangeFox Recovery Project
-# SPDX-License-Identifier: GPL-3.0-or-later
 #
 
 # -----------------------------------------------------------------------------
