3dcf58e4c666cccefcbc709eb792b4fd3fd1e956
QCerberusQ <113352008+QCerberusQ@users.noreply.github.com>
2026-04-29 16:15:36 +0300

    Update virtual_ab_ota product inheritance

--- device.mk
+++ device.mk
@@ -6,7 +6,7 @@
 # -----------------------------------------------------------------------------
 # Base inherits
 # -----------------------------------------------------------------------------
-$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
